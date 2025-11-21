10bac5c4a7f35c971205cd015f5abe43fe5733df
adkadt <adamkb2004@gmail.com>
2025-11-20 20:26:40 -0500

    header file

--- handshake/objectHandler.cpp
+++ handshake/objectHandler.cpp
@@ -1,96 +1,17 @@
-#include "main.h"
 #include <cstdlib>//dynamic memory allocation
 #include <cstring>//string manipulation AUTO
+#include <objectHandler.h>
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "struct_overwrite_TEST_3");
-
-	pros::lcd::register_btn1_cb(on_center_button);
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-
-//global struct array that will be used to store ObjectData structs
-//this represents all detected objects in a single frame
-//this array of structs will be overwritten each time a new frame of dected objects is sent
-
-
-
-typedef struct ObjectData{
-	float confidencevalue;
-	int xcenter;
-	int ycenter;
-	int classid;
-	struct ObjectData* next;
-}ObjectData;
-
-ObjectData* insert_node(ObjectData* root,ObjectData* node){
-	if(root != NULL)
-		root->next = node;
-	return node;
-}//end of insert_node() function
-
-ObjectData* objects_in_frame(){
+GamePieceData* objects_in_frame(){
 	//input buffer read to from stdin
 	char inputbuffer[1200];
-	ObjectData* head = NULL;
+	GamePieceData* head = NULL;
 
 	if(fgets(inputbuffer,sizeof(inputbuffer),stdin) != NULL){
 		//initialize root
-		ObjectData* root = NULL;
+		GamePieceData* root = NULL;
 		//initalize node
-		ObjectData* node = (ObjectData*)malloc(sizeof(ObjectData));
+		GamePieceData* node = (GamePieceData*)malloc(sizeof(GamePieceData));
 		if(node != NULL)
 			node->next = NULL;
 		else
@@ -128,7 +49,7 @@ ObjectData* objects_in_frame(){
                 break;
 
 			//initalize node
-            ObjectData* node = (ObjectData*)malloc(sizeof(ObjectData));
+            GamePieceData* node = (GamePieceData*)malloc(sizeof(GamePieceData));
             if(node != NULL)
                 node->next = NULL;
             else
@@ -163,8 +84,8 @@ ObjectData* objects_in_frame(){
 
 void autonomous() {
 	while(1){
-		ObjectData* root = objects_in_frame();
-		ObjectData* temproot = root;
+		GamePieceData* root = objects_in_frame();
+		GamePieceData* temproot = root;
         int objcount=1;
 		//constantly output overwritten linkedlist to pi terminal
 		while(root != NULL){
@@ -176,7 +97,7 @@ void autonomous() {
 
 		//free linked list
 		while (temproot != NULL){
-			ObjectData* freeroot = temproot;
+			GamePieceData* freeroot = temproot;
 			temproot = temproot->next;
 			free(freeroot);
 		}
---
+++ handshake/objectHandler.h
@@ -0,0 +1,19 @@
+#ifndef OBJECT_HANDLER
+#define OBJECT_HANDLER
+
+enum GamePiece {
+    RED_BALL,
+    BLUE_BALL,
+    MID_GOAL,
+    LONG_GOAL
+};
+
+typedef struct GamePieceData{
+	int x;
+	int y;
+	int conf;
+}GamePieceData;
+
+GamePieceData getObject(GamePiece gamePiece);
+
+#endif
