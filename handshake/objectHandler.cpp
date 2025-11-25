54a2edbe92e247d8065e29158d8433739ba47447
Hakim <162640038+Hakim-Abdurrashid@users.noreply.github.com>
2025-11-25 14:52:20 -0500

    WORKING debug version

--- handshake/objectHandler.cpp
+++ handshake/objectHandler.cpp
@@ -1,138 +1,191 @@
-#include <cstdlib>//dynamic memory allocation
 #include <cstring>//string manipulation AUTO
-#include <objectHandler.h>
+#include "objectHandler.h"
 #include <iostream>
 
-GamePieceData* objects_in_frame(){
-	//input buffer read to from stdin
-	char inputbuffer[1200];
-	GamePieceData* head = NULL;
+GamePieceArray init_array(){
+    //return if any statement fails
+    GamePieceArray failedinitarray;
+    //size can contain 1 frame of all detected objects
+    char inputbuffer[106];
 
-	if(fgets(inputbuffer,sizeof(inputbuffer),stdin) != NULL){
-		//initialize root
-		GamePieceData* root = NULL;
-		//initalize node
-		GamePieceData* node = (GamePieceData*)malloc(sizeof(GamePieceData));
-		if(node != NULL)
-			node->next = NULL;
-		else
-			return NULL;
-		//initialize first node mandatory for strtok()
+    //begin serial handshake
+    printf("A\n");
+    fflush(stdout);
 
-		//tokenize inputbuffer
+    if(fgets(inputbuffer,sizeof(inputbuffer),stdin) != NULL){
+        GamePieceArray framearray;
+        //temporarily store GamePieceData
+        int classidparse;
+        int xparse;
+        int yparse;
+        float confparse;
+
+        //tokenize inputbuffer
+        //class_id
 		char* token = strtok(inputbuffer,",|\n");
-		//initialize class_id field
-		sscanf(token,("%d"),&node->classid);
+		//store class_id
+        if((sscanf(token,("%d"),&classidparse))!=1)
+            return failedinitarray;
 
+        //x_center
+		token = strtok(NULL,",|\n");//115
+		if((sscanf(token,("%d"),&xparse))!=1)
+            return failedinitarray;
+        //y_center
 		token = strtok(NULL,",|\n");
-		//initialize xcenter field
-		sscanf(token,("%d"),&node->xcenter);
-
+		if((sscanf(token,("%d"),&yparse))!=1)
+            return failedinitarray;
+        //confidence value
 		token = strtok(NULL,",|\n");
-		//initialize ycenter field
-		sscanf(token,("%d"),&node->ycenter);
+		if((sscanf(token,("%f"),&confparse))!=1)
+            return failedinitarray;
 
-		token = strtok(NULL,",|\n");
-		//initialize confidencevalue field
-		sscanf(token,("%f"),&node->confidencevalue);
+        GamePieceData framedata={.x=xparse,.y=yparse,.conf=confparse};
 
-		//insert node to linkedlist
-		root = insert_node(root,node);
-
-		//store linkedlist head
-		head = root;
+		switch(classidparse){
+            case RED_BALL:
+                framearray[RED_BALL_INDEX]=framedata;
+                break;
+            case BLUE_BALL:
+                framearray[BLUE_BALL_INDEX]=framedata;
+                break;
+            case LONG_GOAL:
+                framearray[LONG_GOAL_INDEX]=framedata;
+                break;
+            case MID_GOAL_1:
+                framearray[MID_GOAL_1_INDEX]=framedata;
+                break;
+            case MID_GOAL_2:
+                framearray[MID_GOAL_2_INDEX]=framedata;
+                break;
+            case MID_GOAL_3:
+                framearray[MID_GOAL_3_INDEX]=framedata;
+                break;
+            case MID_GOAL_4:
+                framearray[MID_GOAL_4_INDEX]=framedata;
+                break;
+            default:
+                return failedinitarray;
+		}
 
 		while(token != NULL){
-		    //next node
-			token = strtok(NULL,",|\n");
-
-			if(token == NULL)
+            //next detected object
+            //class_id
+            if((token = strtok(NULL,",|\n"))==NULL)
                 break;
+            //store class_id
+            if((sscanf(token,("%d"),&classidparse))!=1)
+                return failedinitarray;
+            //x_center
+            token = strtok(NULL,",|\n");
+            if((sscanf(token,("%d"),&xparse))!=1)
+                return failedinitarray;
+            //y_center
+            token = strtok(NULL,",|\n");
+            if((sscanf(token,("%d"),&yparse))!=1)
+                return failedinitarray;
+            //confidence value
+            token = strtok(NULL,",|\n");
+            if((sscanf(token,("%f"),&confparse))!=1)
+                return failedinitarray;
 
-			//initalize node
-            GamePieceData* node = (GamePieceData*)malloc(sizeof(GamePieceData));
-            if(node != NULL)
-                node->next = NULL;
-            else
-                return NULL;
+            framedata={.x=xparse,.y=yparse,.conf=confparse};
 
-            //initialize class_id field
-			sscanf(token,("%d"),&node->classid);
-
-			token = strtok(NULL,",|\n");
-			//initialize xcenter field
-			sscanf(token,("%d"),&node->xcenter);
-
-			token = strtok(NULL,",|\n");
-			//initialize ycenter field
-			sscanf(token,("%d"),&node->ycenter);
-
-			token = strtok(NULL,",|\n");
-			//initialize confidencevalue field
-			sscanf(token,("%f"),&node->confidencevalue);
-
-			//insert node to linkedlist
-			root = insert_node(root,node);
-		}
-	}else{
-		//no detected objects due to failed fgets()
-		printf("No Objects Detected");
-		return NULL;
-	}
-
-	return head;
-} //end of objects_in_frame() function
-
-void autonomous() {
-	while(1){
-		GamePieceData* root = objects_in_frame();
-		GamePieceData* temproot = root;
-        int objcount=1;
-		//constantly output overwritten linkedlist to pi terminal
-		while(root != NULL){
-			printf("\n___%d___\n\nClass_id:\t%d\nX Center:\t%d\nY Center:\t%d\nConfidence:\t%0.2f\n"
-            ,objcount,root->classid,root->xcenter,root->ycenter,root->confidencevalue);
-            objcount++;
-			root = root->next;
-		}
-
-		//free linked list
-		while (temproot != NULL){
-			GamePieceData* freeroot = temproot;
-			temproot = temproot->next;
-			free(freeroot);
-		}
-	}
+            switch(classidparse){
+                case RED_BALL:
+                    framearray[RED_BALL_INDEX]=framedata;
+                    break;
+                case BLUE_BALL:
+                    framearray[BLUE_BALL_INDEX]=framedata;
+                    break;
+                case LONG_GOAL:
+                    framearray[LONG_GOAL_INDEX]=framedata;
+                    break;
+                case MID_GOAL_1:
+                    framearray[MID_GOAL_1_INDEX]=framedata;
+                    break;
+                case MID_GOAL_2:
+                    framearray[MID_GOAL_2_INDEX]=framedata;
+                    break;
+                case MID_GOAL_3:
+                    framearray[MID_GOAL_3_INDEX]=framedata;
+                    break;
+                case MID_GOAL_4:
+                    framearray[MID_GOAL_4_INDEX]=framedata;
+                    break;
+                default:
+                    return failedinitarray;
+            }
+        }//end of while
+        return framearray;
+    }else{
+        return failedinitarray;
+    }
 }
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
-
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+/*returns an array of std::optional elements of enum GamePiece class COUNT size.
+each std::optional element wraps a GamePieceData structure.
+see cpprefernce std::optional webpage for member functions.
+*/
+//may be used for pre-processing the array before it
+//is returned to caller
+GamePieceArray get_obj(){
+	return init_array();
 }
+
+int main(void){
+    GamePieceArray giveframearray = get_obj();
+
+    if(giveframearray[RED_BALL_INDEX].has_value()){
+        printf("\ndetected red ball\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[RED_BALL_INDEX].value()).x,
+           (giveframearray[RED_BALL_INDEX].value()).y,
+           (giveframearray[RED_BALL_INDEX].value()).conf);
+    }else
+        printf("\nno red ball found\n");
+    if(giveframearray[BLUE_BALL_INDEX].has_value()){
+        printf("\ndetected blue ball\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[BLUE_BALL_INDEX].value()).x,
+           (giveframearray[BLUE_BALL_INDEX].value()).y,
+           (giveframearray[BLUE_BALL_INDEX].value()).conf);
+    }else
+        printf("\nno blue ball found\n");
+    if(giveframearray[LONG_GOAL_INDEX].has_value()){
+        printf("\ndetected long goal\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[LONG_GOAL_INDEX].value()).x,
+           (giveframearray[LONG_GOAL_INDEX].value()).y,
+           (giveframearray[LONG_GOAL_INDEX].value()).conf);
+    }else
+        printf("\nno long goal found\n");
+    if(giveframearray[MID_GOAL_1_INDEX].has_value()){
+        printf("\ndetected mid goal 1\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[MID_GOAL_1_INDEX].value()).x,
+           (giveframearray[MID_GOAL_1_INDEX].value()).y,
+           (giveframearray[MID_GOAL_1_INDEX].value()).conf);
+    }else
+        printf("\nno mid goal 1 found\n");
+    if(giveframearray[MID_GOAL_2_INDEX].has_value()){
+        printf("\ndetected mid goal 2\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[MID_GOAL_2_INDEX].value()).x,
+           (giveframearray[MID_GOAL_2_INDEX].value()).y,
+           (giveframearray[MID_GOAL_2_INDEX].value()).conf);
+    }else
+        printf("\nno mid goal 2 found\n");
+    if(giveframearray[MID_GOAL_3_INDEX].has_value()){
+        printf("\ndetected mid goal 3\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[MID_GOAL_3_INDEX].value()).x,
+           (giveframearray[MID_GOAL_3_INDEX].value()).y,
+           (giveframearray[MID_GOAL_3_INDEX].value()).conf);
+    }else
+        printf("\nno mid goal 3 found\n");
+    if(giveframearray[MID_GOAL_4_INDEX].has_value()){
+        printf("\ndetected mid goal 4\nx:\t%d\ny:\t%d\nconfidence value:\t%f\n",
+           (giveframearray[MID_GOAL_4_INDEX].value()).x,
+           (giveframearray[MID_GOAL_4_INDEX].value()).y,
+           (giveframearray[MID_GOAL_4_INDEX].value()).conf);
+    }else
+        printf("\nno mid goal 4 found\n");
+    return 0;
+}//end of main() function
+
+//0,150,200,0.95|1,320,100,0.88|2,480,350,0.79|3,50,450,0.92|4,600,50,0.85|5,400,250,0.72|6,250,50,0.98\n
