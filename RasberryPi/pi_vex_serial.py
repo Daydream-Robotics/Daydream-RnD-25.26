a9f61faa453972cc3dc2abf103447394d7c78547
adkadt <adam@kbrandt.xyz>
2026-01-23 13:14:37 -0500

    bug fixes

--- RasberryPi/pi_vex_serial.py
+++ RasberryPi/pi_vex_serial.py
@@ -14,8 +14,9 @@ try:
         timeout=1
     )
     #clear input stream
-    ser.flushInput()
+    # ser.flushInput()
     #print success message to console
+    # ser.write('\n'.encode('utf-8'))
     print(f"Serial port {SERIAL_PORT} opened SUCCESSFULLY.")
 except serial.SerialException as e:
     #print error exception to console
@@ -39,9 +40,12 @@ def run_cnn_and_get_output():
 
         if i > 0:
             data_string += "|"
-        data_string += f"{class_id},{int(object['x'])},{int(object['y'])},{object['conf']:.2f}"
+        data_string += f"{class_id},{object['x']:.2f},{object['y']:.2f},{object['conf']:.2f}"
+        object_found[class_id] = True
 
-    return objects
+        print(f"Object: {class_id},{object['x']:.2f},{object['y']:.2f},{object['conf']:.2f}")
+
+    return data_string
 
 def main():
     REQ_CHECK_TIME = 0.5
@@ -54,15 +58,16 @@ def main():
         while not (time.time() - pre_req_time > REQ_CHECK_TIME):
             if ser.in_waiting > 0:
                 #read and store '\n' terminated line from input stream
-                input_string = ser.readline().decode('utf-8').strip()
-                print(input_string)
+                input_string = ser.readline().decode('utf-8', errors='ignore').strip()
+                print(f"VEX: <{input_string}>")
 
                 # checks for request character
-                if input_string == 'A':
+                if 'soutA' in input_string:
                     # send data to VEX brain if a request was recieved
                     try:
                         payload = (data_string + '\n').encode('utf-8')
                         ser.write(payload)
+                        print(f"{time.time()} Wrote: <{data_string}>")
                     except serial.SerialTimeoutException as e:
                         print(f"Error writing to serial port: {e}")
                     # break from loop
