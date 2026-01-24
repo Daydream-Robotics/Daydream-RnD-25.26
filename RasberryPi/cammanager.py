04caddca3227676c2da67e5414ca882059067ff1
adkadt <adamkb2004@gmail.com>
2026-01-23 22:57:16 -0500

    rotated wrong direction

--- RasberryPi/cammanager.py
+++ RasberryPi/cammanager.py
@@ -7,7 +7,7 @@ import numpy as np
 picam2 = Picamera2() # creates a camera object
 config = picam2.create_video_configuration(
     main={"format": "RGB888"},
-    # main={"size": (640, 640), "format": "RGB888"},
+    main={"size": (640, 640), "format": "RGB888"},
     # transform=Transform.Rot90,  # Rotates hardware read-out
     controls={"FrameRate": 120}
 )
@@ -18,6 +18,6 @@ picam2.start() # starts camera object
 # this just returns the most recent frame
 def getCamPIL():
     image = picam2.capture_image("main")
-    image = image.rotate(90, expand=True)
+    image = image.rotate(-90, expand=True)
     return image
 
