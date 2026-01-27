6f923017022e58373b56c6135bcd9f8a82b97b71
adkadt <adamkb2004@gmail.com>
2026-01-27 18:48:07 -0500

    updated autostart

--- RaspberryPi/startVex.sh
+++ RaspberryPi/startVex.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
 source ~/cam_venv/bin/activate
-python /home/adam/Daydream-RnD-25.26/RaspberryPi/pi_vex_serial.py > /home/adam/PI.out 2>&1
+python3 ${BASEDIR}/pi_vex_serial.py > ~/Pi.out 2>&1
